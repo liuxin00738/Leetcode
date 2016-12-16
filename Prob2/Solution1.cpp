4ac93ecb12e9eb11994770a089eb569ba4fa6f1e
liuxin00738 <liuxin00738@gmail.com>
2016-12-15 22:51:10 -0500

    add problem2 and solution using new to allocate memory

---
+++ Prob2/Solution1.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        if(l1==nullptr)  return l2;
+        if(l2==nullptr)  return l1;
+
+	ListNode* result (new ListNode(0));
+        ListNode* resultSaver=result;
+	int overflow=0;
+	
+        while(l1 != nullptr || l2 != nullptr){
+		    int tempSum=0;
+		    tempSum+=overflow;
+		    if(l1  != nullptr) tempSum+=l1->val;
+		    if(l2  != nullptr) tempSum+=l2->val;
+
+		    overflow= (tempSum>9)? 1 : 0;
+		    tempSum= (tempSum>9)? (tempSum-10) : tempSum;
+		
+		    ListNode* tempPtr =new ListNode(tempSum);
+		    result->next=tempPtr;	
+		    result=result->next;
+		    if(l1 != nullptr) l1=l1->next;
+		    if(l2 != nullptr) l2=l2->next;
+	
+	} 
+	// deal with case when need to have one more bit
+        if(overflow==1){
+		ListNode* tempPtr =new ListNode(overflow);
+		result->next=tempPtr;
+	}
+	return resultSaver->next;
+
+    }
+    
+};
