bfd0dc5e11fb2c93b3827fc09f00383c24134d85
liuxin00738 <liuxin00738@gmail.com>
2016-12-21 22:44:56 -0500

    add solution to question DogSearch.cpp

--- Vecna/DogSearch.cpp
+++ Vecna/DogSearch.cpp
@@ -11,26 +11,27 @@
 // then discuss the ambiguity in the problem description, and
 // reword it to remove the ambiguity in the space provided below.
 //
+// New Description:
+// Count the number of times the lowercase substring "dog" appears in some input text.
 //
 //
-//
-//
-//
-//
+// 
 
 int main() {
   std::string input = "The Dogman was no ordinary dog, nor man, but rather a peculiar dog-like man who barked like a dog, and panted like a dog, he even ate like a dog.  He owned a dog named Doglips, and interestingly enough, his favorite food was hotdogs.";
-  int index = -1;
+  int index = 0;
   int count = 0;
   std::cout << "Counting dogs:";
   do {
-    index = input.whereIs("dog");
+    index = input.find("dog",index );
     if (index != -1) {
       count++;
       std::cout << index << " ";
+      index+=3;
     }
   } while (index != -1);
-  std::cout << "The word \"dog\" appears " << count << " times." << std::endl;
+  std::cout << std::endl;  
+  std::cout << "The lowercase substring \"dog\" appears " << count << " times." << std::endl;
 }
 
 
