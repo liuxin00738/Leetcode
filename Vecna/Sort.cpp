256ebb0e4ff1322726177fbbdbdb63d74d0991f1
liuxin00738 <liuxin00738@gmail.com>
2016-12-21 21:48:19 -0500

    solve question 1, Sort.cpp

--- Vecna/Sort.cpp
+++ Vecna/Sort.cpp
@@ -5,12 +5,11 @@
 // This code will sort the numbers in the array.
 //
 // Task: Modify the algorithm to reverse the sort, then comment on its efficiency in the space 
+// I Modified line 27, 30, 36
 // provided below:
-//
-//
-//
-//
-//
+// Time complexity is O(n^2) 
+// because (1+2+...+n=n*(n+1)/2)
+// Space complexity is O(n)
 
 int main() {
   int unsorted[100] = {};
@@ -25,16 +24,16 @@ int main() {
   std::cout << std::endl;
   int sorted[100] = {};
   for (int i = 0; i < 100; i++) {
-    int hi = -1;
+    int hi = 101; //initialized to a number bigger than all other numbers
     int hiIndex = -1;
     for (int j = 0; j < 100; j++) {
-      if (unsorted[j] > hi) {
+      if (unsorted[j] <hi) { //keep record the smallest of the rest
         hi = unsorted[j];
         hiIndex = j;
       }
     }
     sorted[i] = hi;
-    unsorted[hiIndex] = -1;
+    unsorted[hiIndex] = 101; // set the sorted number to a number bigger than all other numbers
   }
   std::cout << "Here are the sorted numbers:" << std::endl;
   for (int i = 0; i < 100; i++) {
