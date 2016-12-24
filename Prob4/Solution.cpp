c65d312a0fc5779ca2158399554e03f2f71fde6e
liuxin00738 <liuxin00738@gmail.com>
2016-12-23 23:35:13 -0500

    add an incorrect solution to leetcode 4

--- Prob4/Solution.cpp
+++ Prob4/Solution.cpp
@@ -1,3 +1,5 @@
+#include <algorithm>
+#include <vector>
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
@@ -21,47 +23,46 @@ public:
         static int totalNums=nums1.size()+nums2.size();
         static int halfOfTotalNums= (totalNums%2==0)? (totalNums/2-1) : (totalNums/2);
         
-        // if has not removed enough elements, do recursive call
-        if((nums1LeftIdx +nums2LeftIdx) < halfOfTotalNums ) < halfOfTotalNums  ){
-            // get the middle number of the two arrays
-            int nums1MiddleIdx=(nums1LeftIdx +nums1RightIdx) /2;
-            int nums1Middle=nums1[nums1MiddleIdx];
+        // exit condition: has remove half of the elements on both sides, then return result
+        if((nums1LeftIdx +nums2LeftIdx) == halfOfTotalNums 
+           && (totalNums-nums1RightIdx-nums2RightIdx)==halfOfTotalNums){
+            double sum=0;
+            for(int i=nums1LeftIdx;i<nums1.size();i++)
+                sum+=nums1[i];
+            for(int i=nums2LeftIdx;i<nums2.size();i++)
+                sum+=nums2[i];
             
-            int nums2MiddleIdx=(nums2LeftIdx +nums2RightIdx) /2;
-            int nums2Middle=nums1[nums2MiddleIdx];
+            return sum/(totalNums-2*halfOfTotalNums);        
+        }
+
+        // get the middle number [middle or right half] of the two arrays
+        int nums1MiddleIdx=(nums1LeftIdx +nums1RightIdx) /2;
+        int nums1Middle=nums1[nums1MiddleIdx];
+            
+        int nums2MiddleIdx=(nums2LeftIdx +nums2RightIdx) /2;
+        int nums2Middle=nums1[nums2MiddleIdx];        
+        
+        // if has not removed enough left elements, do recursive call to remove 
+        {
             
             // if nums2.middle is bigger
             if(nums2Middle >= nums1Middle){                
-                // if has odd elements,  add extra 1
-                // if has even elements, do not add 1        
-                nums2RightIdx=( (nums2RightIdx-nums2LeftIdx)%2 ==0)? nums2MiddleIdx : (nums2MiddleIdx+1);                                  
-                nums1LeftIdx= nums1MiddleIdx;                 
+                // if has odd elements,  add extra 1       
+                if((totalNums-nums1RightIdx-nums2RightIdx) < halfOfTotalNums ) 
+                    nums2RightIdx=( (nums2RightIdx-nums2LeftIdx)%2 ==0)? nums2MiddleIdx : (nums2MiddleIdx+1);                                  
+                if((nums1LeftIdx +nums2LeftIdx) < halfOfTotalNums ) 
+                    nums1LeftIdx= nums1MiddleIdx;                 
             }
             // if nums1.middle is smaller
             else{
-                nums1RightIdx=( (nums1RightIdx-nums1LeftIdx)%2 ==0)? nums1MiddleIdx : (nums1MiddleIdx+1);                                  
-                nums2LeftIdx= nums2MiddleIdx;              
+                if((totalNums-nums1RightIdx-nums2RightIdx) < halfOfTotalNums ) 
+                    nums1RightIdx=( (nums1RightIdx-nums1LeftIdx)%2 ==0)? nums1MiddleIdx : (nums1MiddleIdx+1);                                  
+                if((nums1LeftIdx +nums2LeftIdx) < halfOfTotalNums ) 
+                    nums2LeftIdx= nums2MiddleIdx;              
             } 
             // do recursive call
-            findMedianSortedArray(nums1, nums2, nums1LeftIdx, nums1RightIdx, nums2LeftIdx, nums2RightIdx);        
+            return findMedianSortedArray(nums1, nums2, nums1LeftIdx, nums1RightIdx, nums2LeftIdx, nums2RightIdx);        
         }
-        
-        // if has removed enough elements, return result
-        else
-        {
-            double result=0;
-            int  numCount=0;
-            for(int i=nums1LeftIdx; i< nums1RightIdx;i++){
-                result+=nums1[nums1LeftIdx];
-                numCount++;
-            }
-            for(int i=nums2LeftIdx; i< nums2RightIdx;i++){
-                result+=nums2[nums2LeftIdx];
-                numCount++;
-            }      
-        
-        }
-            
         
     }
     
