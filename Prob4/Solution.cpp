8321f2d96f00d043950c179f23e91ce943740001
liuxin00738 <liuxin00738@gmail.com>
2016-12-24 13:45:50 -0500

    add a reference solution to leetcode #4

--- Prob4/Solution.cpp
+++ Prob4/Solution.cpp
@@ -20,20 +20,15 @@ public:
         
         if (totalNums%2 == 1)
             return findKth(nums1, nums2, nums1LeftIdx, nums2LeftIdx, totalNums/2+1);
-        
-        nums1LeftIdx=0;
-        nums2LeftIdx=0; 
+
         int leftMiddle=findKth(nums1, nums2, nums1LeftIdx, nums2LeftIdx, totalNums/2+1);
-        
-        nums1LeftIdx=0;
-        nums2LeftIdx=0; 
         int rightMiddle=findKth(nums1, nums2, nums1LeftIdx, nums2LeftIdx, totalNums/2);
         
         return  (leftMiddle+rightMiddle)/2.0;      
     }
 
     int findKth(vector<int>& nums1, vector<int>& nums2 
-           , int& nums1LeftIdx, int& nums2LeftIdx, int k) {
+           , int nums1LeftIdx, int nums2LeftIdx, int k) {
         
         //cout<< nums1LeftIdx<<" "<<nums2LeftIdx<<" "<<k<<endl;
         if(nums1LeftIdx==nums1.size())
@@ -44,8 +39,8 @@ public:
         
         if(k==1)
             return min(nums1[nums1LeftIdx], nums2[nums2LeftIdx]);       
-        int temmp=min(nums1.size()-nums1LeftIdx, nums2.size()-nums2LeftIdx) ;
-        int step=min(k/2, temmp);
+        int maxStep=min(nums1.size()-nums1LeftIdx, nums2.size()-nums2LeftIdx) ;
+        int step=min(k/2, maxStep);
         //cout<<"step "<<step<<endl;
 
         int nums1Value=nums1[nums1LeftIdx+step-1]; 
